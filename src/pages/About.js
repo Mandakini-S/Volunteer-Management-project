9234b582cb8135c270e57c1742e7d524cd446ce8
mandakini <mandakinisapkota@gmail.com>
2023-02-14 23:01:45 +0545

    Added CSS

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,12 +3,15 @@ import Footer from '../components/Footer.js';
 import Navbar from "../components/Navbar1.js";
 import Signup1 from './Orgsignup.js';
 // import Footer from '../components/Footer.js';
+import "../pages/About.css";
 
 const About = () => {
   return (
     <div>
       <Navbar/>
       Will update soon...
+      <div className='foot'> <Footer/></div>
+     
      
     </div>
     
